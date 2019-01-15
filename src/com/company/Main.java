a041d332d792a3ae21df3c4e129234f6aecd8e0e
Unknown <dskinner@uark.edu>
2019-01-15 15:51:36 -0600

    insertion sort complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,55 @@
 package com.company;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
+
+    public static void mergeSort(int[] B) {
+
+
+    }
+
+
+    public static void insertionSortNonDecreasing(int[] B ){
+
+        System.out.println("Insertion Sort");
+        System.out.print("unsorted: ");
+
+        for (int bB:
+             B)
+        {
+            System.out.print(bB + " ");
+        }
+        System.out.println();
+
+        // j is the position of the current position being evaluated
+        // i is the position of the (current position being evaluated - 1)
+
+        for (int j = 1; j < B.length; j++)
+        {
+            int key = B[j];
+
+            // insert A[j] into the sorted sequence A[1..j - 1]
+            int i = j -1;
+
+            //to make insertion sort non-increasing swap "B[i] > key" -> "B[i] < key"
+            while(i >= 0 && B[i] > key)
+            {
+                // swap values for B[i + 1] + B[i]
+                int temp0 = B[i];
+                B[i] = B[i +1];
+                B[i +1] = temp0;
+
+                i--;
+                B[i + 1] = key;
+            }
+        }
+
+        System.out.print("sorted: ");
+
+        for (int aB : B)
+        {
+            System.out.print(aB + " ");
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -12,36 +59,20 @@ public class Main {
 
         //integer array
 
-        int A[] =  {2,1};
-        int current;
+        int A[] =  {2,1,4,5,6,3};
 
-        for (int j = 1; j < A.length ; j++)
-        {
-            current = A[j];
-
-            int i = j -1;
-
-            while(current < A[i])
-            {
-                //swap current and A[i];
-            }
-        }
-
-        for (int j = 0; j < A.length; j++)
-        {
-            System.out.println(A[j]);
-        }
-
-        if(A[1] < A[0])
-        {
-            int temp0 = A[0];
-            A[0] = A[1];
-            A[1] = temp0;
-        }
-
-        for (int j = 0; j < A.length; j++)
-        {
-            System.out.println(A[j]);
-        }
+        insertionSortNonDecreasing(A);
+//
+//        if(A[1] < A[0])
+//        {
+//            int temp0 = A[0];
+//            A[0] = A[1];
+//            A[1] = temp0;
+//        }
+//
+//        for (int j = 0; j < A.length; j++)
+//        {
+//            System.out.println(A[j]);
+//        }
     }
 }
