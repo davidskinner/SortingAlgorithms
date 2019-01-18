22c851872666d049aa733779b65dd5f539591815
Unknown <dskinner@uark.edu>
2019-01-17 18:07:49 -0600

    restructuring

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,72 +3,13 @@ package com.company;
 import java.util.Random;
 
 public class Main {
-
-    //Best: 0(n)
-    //Worst: omega(n^2)
-    public static int[] bubbleSort(int[] intArray)
-    {
-        for (int i = 0; i < intArray.length-1; i++)
-        {
-            for (int j = intArray.length-1; j > 0; j--)
-            {
-                if(intArray[j] < intArray[j-1])
-                {
-                    //swap intArray[j] with intArray[j-1]
-                    int temp = intArray[j];
-                    intArray[j] = intArray[j-1];
-                    intArray[j-1] = temp;
-                }
-            }
-        }
-        return intArray;
-    }
-
-    //Best: O(n log(n))
-    //Worst: omega(n log(n))
-    //better than insertion sort for large datasets
-    public static int[] mergeSort(int[] intArray,int p,int q, int r) {
-
-        int n1 = q - p;
-        int n2 = r - q;
-
-        int[] left = new int[n1];
-        int[] right = new int[n2];
-
-        for (int i = 0; i < n1; i++)
-        {
-            left[i] = intArray[p + i];
-        }
-
-        for (int i = 0; i < n1; i++)
-        {
-            right[i] = intArray[q + i];
-        }
-
-        int i = 0;
-        int j = 0;
-
-//        for (int k = p; k < r; k++)
-//        {
-//            if(left[i] <= right[j])
-//            {
-//                intArray[k] = left[i];
-//                i++;
-//            }
-//            else{
-//                intArray[k] = right[j];
-//                j++;
-//            }
-//        }
-        return intArray;
-    }
-
-
-
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
         int[] A = new int[] {2,1,4,5,6,3,8,9};
+        Sort sort = new Sort();
 
-        InsertionSort insertionSort = new InsertionSort(A);
+        sort.insertionSort(A);
+        sort.bubbleSort(A);
+
     }
 }
--- src/com/company/Sort.java
+++ src/com/company/Sort.java
@@ -1,13 +1,14 @@
 package com.company;
 
-public abstract class Sort implements ISort
+public class Sort
 {
 
-	public static void print(String name, String unsortedArrayString, String sortedArrayString)
+	public static void print(String name, int[] unsortedArray, int[] sortedArray)
 	{
 		System.out.println(name);
-		System.out.println(unsortedArrayString);
-		System.out.println(sortedArrayString);
+		System.out.println(buildString(unsortedArray));
+		System.out.println(buildString(sortedArray));
+		System.out.println();
 	}
 
 	public static String buildString(int[] A)
@@ -21,6 +22,98 @@ public abstract class Sort implements ISort
 		return sb.toString();
 	}
 
-	public abstract int[] sort();
+	public int[] insertionSort(int[] A)
+	{
+		//copy array
+		int[] tempArray = A.clone();
 
+		String name = "Insertion Sort";
+
+		// i is the current position being evaluated
+		// j is the (current position being evaluated - 1)
+		for (int i = 1; i < tempArray.length; i++)
+		{
+			int current = tempArray[i];
+			int j = i - 1;
+
+			//to make insertion sort non-increasing swap "array[j] > current" -> "array[j] < current"
+			while (j >= 0 && tempArray[j] > current)
+			{
+				// swap values for array[j + 1] and array[j]
+				int temp = tempArray[j];//
+				tempArray[j] = tempArray[j + 1];
+				tempArray[j + 1] = temp;
+
+				//move to the left in the sorted array
+				j--;
+				tempArray[j + 1] = current;
+			}
+		}
+
+		print(name,A,tempArray);
+		return tempArray;
+	}
+
+	//Best: 0(n)
+	//Worst: omega(n^2)
+	public int[] bubbleSort(int[] A)
+	{
+		int[] tempArray = A.clone();
+		String name = "Bubble Sort";
+
+		for (int i = 0; i < tempArray.length-1; i++)
+		{
+			for (int j = tempArray.length-1; j > 0; j--)
+			{
+				if(tempArray[j] < tempArray[j-1])
+				{
+					//swap tempArray[j] with tempArray[j-1]
+					int temp = tempArray[j];
+					tempArray[j] = tempArray[j-1];
+					tempArray[j-1] = temp;
+				}
+			}
+		}
+		print(name,A,tempArray);
+		return tempArray;
+	}
+
+	//Best: O(n log(n))
+	//Worst: omega(n log(n))
+	//better than insertion sort for large datasets
+	public static int[] mergeSort(int[] intArray,int p,int q, int r) {
+
+		int n1 = q - p;
+		int n2 = r - q;
+
+		int[] left = new int[n1];
+		int[] right = new int[n2];
+
+		for (int i = 0; i < n1; i++)
+		{
+			left[i] = intArray[p + i];
+		}
+
+		for (int i = 0; i < n1; i++)
+		{
+			right[i] = intArray[q + i];
+		}
+
+		int i = 0;
+		int j = 0;
+
+		//        for (int k = p; k < r; k++)
+		//        {
+		//            if(left[i] <= right[j])
+		//            {
+		//                intArray[k] = left[i];
+		//                i++;
+		//            }
+		//            else{
+		//                intArray[k] = right[j];
+		//                j++;
+		//            }
+		//        }
+		return intArray;
+	}
 }
